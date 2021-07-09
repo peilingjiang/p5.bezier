17ead6143a34fa5aad42e5709458d2ff52c90d71
Peiling Jiang <25191575+peilingjiang@users.noreply.github.com>
2021-07-09 10:03:51 -0400

    format

--- examples/server.js
+++ examples/server.js
@@ -2,13 +2,15 @@
 // NYU ITP/IMA 2020
 
 // Create server
-let port = process.env.PORT || 8000;
-let express = require('express');
-let app = express();
-let server = require('http').createServer(app).listen(port, function () {
-  console.log('Server listening at port: ', port);
-});
+let port = process.env.PORT || 8000
+let express = require('express')
+let app = express()
+let server = require('http')
+  .createServer(app)
+  .listen(port, function () {
+    console.log('Server listening at port: ', port)
+  })
 
 // Take argv and process into address
-var myArgs = process.argv.slice(2);
-app.use(express.static(myArgs[0]));
+var myArgs = process.argv.slice(2)
+app.use(express.static(myArgs[0]))
